3826f7f7b8fa6fa10610caa23e144ff27ee88b8c
Nick Sillik <nick@sillik.org>
2025-11-22 08:35:25 -0500

    Initial Commit

---
+++ VanDammeTests/VanDammeTests.swift
@@ -0,0 +1,16 @@
+//
+//  VanDammeTests.swift
+//  VanDammeTests
+//
+//  Created by Nick Sillik on 11/22/25.
+//
+
+import Testing
+
+struct VanDammeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
